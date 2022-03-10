99b0e79e9dbb973301d596223fb8451b96d40b00
Nitin-Sijariya12 <43565686+Nitin-Sijariya12@users.noreply.github.com>
2022-03-10 15:47:19 +0530

    Update HelloControllerTest.java

--- src/test/java/com/mkyong/HelloControllerTest.java
+++ src/test/java/com/mkyong/HelloControllerTest.java
@@ -1,20 +1,15 @@
 package com.mkyong;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloControllerTest {
 
     @Test
     public void hello_ok() throws Exception {
-             Assert.assertEquals("Nitin", "Nitin");
+        Assertions.assertEquals(1, 1);
     }
 
 }
